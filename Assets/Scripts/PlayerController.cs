6821edfd692fbb8302dcc2cc32746d38d9e51611
a57654 <a57654@alunos.ipb.pt>
2025-06-02 22:01:25 +0100

    Fim Lesson 3
    Game finalizado!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,20 +7,31 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
     }
 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
-        {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {   
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
@@ -29,10 +40,17 @@ public class PlayerController : MonoBehaviour
         if (collission.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
-        } else if (collission.gameObject.CompareTag("Obstacle"))
-        {
+            dirtParticle.Play();
+        } 
+        else if (collission.gameObject.CompareTag("Obstacle"))
+        {        
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,7 +4,7 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(25,0,0);
+    private Vector3 spawnPos = new Vector3(35,0,0);
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerController playerControllerScript;
